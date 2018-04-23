9d545e1d4f1eb7fbc19169d7f6231572dd0e71bd
gayathriajith <38522244+gayathriajith@users.noreply.github.com>
2018-04-23 14:04:58 +0530

    Return a string where for every character in the original there are three characters
    paper_doll('Hello') --> 'HHHeeellllllooo'
    paper_doll('Mississippi') --> 'MMMiiissssssiiippppppiii'

---
+++ three_instead_one.py
@@ -0,0 +1,12 @@
+def paper_doll(text):
+    a =0
+    list1 = []
+    for i in text:
+        list1.append(i*3)
+    print(list1)
+        
+    str1 = ''
+    for j in list1:
+        str1+=j
+    print(str1)
+        
